53f3688d3ebecc1151b9818e8bab4994d2aee7d6
Darya <70164413+Darya2311@users.noreply.github.com>
2021-10-06 17:00:24 +0300

    add third homework

---
+++ homework 3/home3.js
@@ -0,0 +1,29 @@
+Array.prototype.myFilter =  function (callbackFunc, thisObject) {
+  let context = this;
+  if (arguments.length > 1) {
+    context = thisObject;
+  };
+  if(typeof callbackFunc !== 'function') {
+    throw new Error('callback is not a function')
+  };
+  let filtered = [];
+  for(let i = 0; i < this.length; i++) {
+    if (callbackFunc.call(context, this[i], i, this)) filtered.push(this[i]);
+  }
+  return filtered;
+};
+
+
+
+
+
+function createDebounceFunction(callbackFunc, ms) {
+  let timeOut;
+  return function () {
+    if(timeOut) {
+      clearTimeout(timeOut);
+    }
+    timeOut = setTimeout(callbackFunc, ms);
+    return timeOut;
+  }
+};
